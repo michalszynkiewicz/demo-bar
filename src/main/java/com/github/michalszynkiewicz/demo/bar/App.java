01c25ed35c65e1b1aa0ad1a067a1a6db6fc2f6d6
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2018-05-24 12:07:34 +0200

    initial

---
+++ src/main/java/com/github/michalszynkiewicz/demo/bar/App.java
@@ -0,0 +1,15 @@
+package com.github.michalszynkiewicz.demo.bar;
+
+import com.github.michalszynkiewicz.demo.foo.MyUtils;
+
+/**
+ *
+ * @author Michal Szynkiewicz, michal.l.szynkiewicz@gmail.com
+ * <br>
+ * Date: 5/24/18
+ */
+public class App {
+    public static void main(String[] args) {
+        MyUtils.printStackTrace();
+    }
+}
